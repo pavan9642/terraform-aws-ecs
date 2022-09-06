0def9d9311f66df4fa1a712b3d4aca500feb8fed
pavan9642 <94094711+pavan9642@users.noreply.github.com>
2022-09-06 13:30:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,112 +1,331 @@
-################################################################################
-# Cluster
-################################################################################
+#vpc#
 
-resource "aws_ecs_cluster" "this" {
-  count = var.create ? 1 : 0
+resource "aws_vpc" "aws-vpc" {
+  cidr_block           = "10.10.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  tags = {
+    Name        = "${var.app_name}-vpc"
+    Environment = var.app_environment
+  }
+}
+#nn#
+resource "aws_internet_gateway" "aws-igw" {
+  vpc_id = aws_vpc.aws-vpc.id
+  tags = {
+    Name        = "${var.app_name}-igw"
+    Environment = var.app_environment
+  }
 
-  name = var.cluster_name
+}
 
-  dynamic "configuration" {
-    for_each = try([var.cluster_configuration], [])
+resource "aws_subnet" "private" {
+  vpc_id            = aws_vpc.aws-vpc.id
+  count             = length(var.private_subnets)
+  cidr_block        = element(var.private_subnets, count.index)
+  availability_zone = element(var.availability_zones, count.index)
 
-    content {
-      dynamic "execute_command_configuration" {
-        for_each = try([configuration.value.execute_command_configuration], [{}])
+  tags = {
+    Name        = "${var.app_name}-private-subnet-${count.index + 1}"
+    Environment = var.app_environment
+  }
+}
 
-        content {
-          kms_key_id = try(execute_command_configuration.value.kms_key_id, null)
-          logging    = try(execute_command_configuration.value.logging, "DEFAULT")
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.aws-vpc.id
+  cidr_block              = element(var.public_subnets, count.index)
+  availability_zone       = element(var.availability_zones, count.index)
+  count                   = length(var.public_subnets)
+  map_public_ip_on_launch = true
 
-          dynamic "log_configuration" {
-            for_each = try([execute_command_configuration.value.log_configuration], [])
+  tags = {
+    Name        = "${var.app_name}-public-subnet-${count.index + 1}"
+    Environment = var.app_environment
+  }
+}
 
-            content {
-              cloud_watch_encryption_enabled = try(log_configuration.value.cloud_watch_encryption_enabled, null)
-              cloud_watch_log_group_name     = try(log_configuration.value.cloud_watch_log_group_name, null)
-              s3_bucket_name                 = try(log_configuration.value.s3_bucket_name, null)
-              s3_bucket_encryption_enabled   = try(log_configuration.value.s3_bucket_encryption_enabled, null)
-              s3_key_prefix                  = try(log_configuration.value.s3_key_prefix, null)
-            }
-          }
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.aws-vpc.id
+
+  tags = {
+    Name        = "${var.app_name}-routing-table-public"
+    Environment = var.app_environment
+  }
+}
+
+resource "aws_route" "public" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.aws-igw.id
+}
+
+resource "aws_route_table_association" "public" {
+  count          = length(var.public_subnets)
+  subnet_id      = element(aws_subnet.public.*.id, count.index)
+  route_table_id = aws_route_table.public.id
+}
+##ecr##
+
+resource "aws_ecr_repository" "aws-ecr" {
+  name = "${var.app_name}-${var.app_environment}-ecr"
+  tags = {
+    Name        = "${var.app_name}-ecr"
+    Environment = var.app_environment
+  }
+}
+##
+resource "aws_iam_role" "ecsTaskExecutionRole" {
+  name               = "${var.app_name}-execution-task-role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+  tags = {
+    Name        = "${var.app_name}-iam-role"
+    Environment = var.app_environment
+  }
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
+  role       = aws_iam_role.ecsTaskExecutionRole.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+}
+###
+resource "aws_ecs_cluster" "aws-ecs-cluster" {
+  name = "${var.app_name}-${var.app_environment}-cluster"
+  tags = {
+    Name        = "${var.app_name}-ecs"
+    Environment = var.app_environment
+  }
+}
+###
+resource "aws_cloudwatch_log_group" "log-group" {
+  name = "${var.app_name}-${var.app_environment}-logs"
+
+  tags = {
+    Application = var.app_name
+    Environment = var.app_environment
+  }
+}
+###
+data "template_file" "env_vars" {
+  template = file("env_vars.json")
+}
+
+resource "aws_ecs_task_definition" "aws-ecs-task" {
+  family = "${var.app_name}-task"
+
+  container_definitions = <<DEFINITION
+  [
+    {
+      "name": "${var.app_name}-${var.app_environment}-container",
+      "image": "${aws_ecr_repository.aws-ecr.repository_url}:latest",
+      "essential": true,
+      "logConfiguration": {
+        "logDriver": "awslogs",
+        "options": {
+          "awslogs-group": "${aws_cloudwatch_log_group.log-group.id}",
+          "awslogs-region": "${var.aws_region}",
+          "awslogs-stream-prefix": "${var.app_name}-${var.app_environment}"
         }
-      }
+      },
+      "portMappings": [
+        {
+          "containerPort": 8080,
+          "hostPort": 8080
+        }
+      ],
+      "cpu": 256,
+      "memory": 512,
+      "networkMode": "awsvpc"
     }
-  }
-
-  dynamic "setting" {
-    for_each = [var.cluster_settings]
-
-    content {
-      name  = setting.value.name
-      value = setting.value.value
-    }
-  }
-
-  tags = var.tags
-}
-
-################################################################################
-# Cluster Capacity Providers
-################################################################################
-
-locals {
-  default_capacity_providers = merge(
-    { for k, v in var.fargate_capacity_providers : k => v if var.default_capacity_provider_use_fargate },
-    { for k, v in var.autoscaling_capacity_providers : k => v if !var.default_capacity_provider_use_fargate }
-  )
-}
-
-resource "aws_ecs_cluster_capacity_providers" "this" {
-  count = var.create && length(merge(var.fargate_capacity_providers, var.autoscaling_capacity_providers)) > 0 ? 1 : 0
-
-  cluster_name = aws_ecs_cluster.this[0].name
-  capacity_providers = distinct(concat(
-    [for k, v in var.fargate_capacity_providers : try(v.name, k)],
-    [for k, v in var.autoscaling_capacity_providers : try(v.name, k)]
-  ))
-
-  dynamic "default_capacity_provider_strategy" {
-    for_each = local.default_capacity_providers
-    iterator = strategy
-
-    content {
-      capacity_provider = try(strategy.value.name, strategy.key)
-      base              = try(strategy.value.default_capacity_provider_strategy.base, null)
-      weight            = try(strategy.value.default_capacity_provider_strategy.weight, null)
-    }
-  }
-
-  depends_on = [
-    aws_ecs_capacity_provider.this
   ]
+  DEFINITION
+
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  memory                   = "512"
+  cpu                      = "256"
+  execution_role_arn       = aws_iam_role.ecsTaskExecutionRole.arn
+  task_role_arn            = aws_iam_role.ecsTaskExecutionRole.arn
+
+  tags = {
+    Name        = "${var.app_name}-ecs-td"
+    Environment = var.app_environment
+  }
 }
 
-################################################################################
-# Capacity Provider - Autoscaling Group(s)
-################################################################################
+data "aws_ecs_task_definition" "main" {
+  task_definition = aws_ecs_task_definition.aws-ecs-task.family
+}
+###
+resource "aws_ecs_service" "aws-ecs-service" {
+  name                 = "${var.app_name}-${var.app_environment}-ecs-service"
+  cluster              = aws_ecs_cluster.aws-ecs-cluster.id
+  task_definition      = "${aws_ecs_task_definition.aws-ecs-task.family}:${max(aws_ecs_task_definition.aws-ecs-task.revision, data.aws_ecs_task_definition.main.revision)}"
+  launch_type          = "FARGATE"
+  scheduling_strategy  = "REPLICA"
+  desired_count        = 1
+  force_new_deployment = true
 
-resource "aws_ecs_capacity_provider" "this" {
-  for_each = { for k, v in var.autoscaling_capacity_providers : k => v if var.create }
-
-  name = try(each.value.name, each.key)
-
-  auto_scaling_group_provider {
-    auto_scaling_group_arn         = each.value.auto_scaling_group_arn
-    managed_termination_protection = try(each.value.managed_termination_protection, null)
-
-    dynamic "managed_scaling" {
-      for_each = try([each.value.managed_scaling], [])
-
-      content {
-        instance_warmup_period    = try(managed_scaling.value.instance_warmup_period, null)
-        maximum_scaling_step_size = try(managed_scaling.value.maximum_scaling_step_size, null)
-        minimum_scaling_step_size = try(managed_scaling.value.minimum_scaling_step_size, null)
-        status                    = try(managed_scaling.value.status, null)
-        target_capacity           = try(managed_scaling.value.target_capacity, null)
-      }
-    }
+  network_configuration {
+    subnets          = aws_subnet.private.*.id
+    assign_public_ip = false
+    security_groups = [
+      aws_security_group.service_security_group.id,
+      aws_security_group.load_balancer_security_group.id
+    ]
   }
 
-  tags = merge(var.tags, try(each.value.tags, {}))
+  load_balancer {
+    target_group_arn = aws_lb_target_group.target_group.arn
+    container_name   = "${var.app_name}-${var.app_environment}-container"
+    container_port   = 8080
+  }
+
+  depends_on = [aws_lb_listener.listener]
+}
+###
+resource "aws_security_group" "service_security_group" {
+  vpc_id = aws_vpc.aws-vpc.id
+
+  ingress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    security_groups = [aws_security_group.load_balancer_security_group.id]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name        = "${var.app_name}-service-sg"
+    Environment = var.app_environment
+  }
+}
+##
+resource "aws_alb" "application_load_balancer" {
+  name               = "${var.app_name}-${var.app_environment}-alb"
+  internal           = false
+  load_balancer_type = "application"
+  subnets            = aws_subnet.public.*.id
+  security_groups    = [aws_security_group.load_balancer_security_group.id]
+
+  tags = {
+    Name        = "${var.app_name}-alb"
+    Environment = var.app_environment
+  }
+}
+##
+resource "aws_security_group" "load_balancer_security_group" {
+  vpc_id = aws_vpc.aws-vpc.id
+
+  ingress {
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  tags = {
+    Name        = "${var.app_name}-sg"
+    Environment = var.app_environment
+  }
+}
+##
+resource "aws_lb_target_group" "target_group" {
+  name        = "${var.app_name}-${var.app_environment}-tg"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = aws_vpc.aws-vpc.id
+
+  health_check {
+    healthy_threshold   = "3"
+    interval            = "300"
+    protocol            = "HTTP"
+    matcher             = "200"
+    timeout             = "3"
+    path                = "/v1/status"
+    unhealthy_threshold = "2"
+  }
+
+  tags = {
+    Name        = "${var.app_name}-lb-tg"
+    Environment = var.app_environment
+  }
+}
+###
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_alb.application_load_balancer.id
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target_group.id
+  }
+}
+###
+
+resource "aws_appautoscaling_target" "ecs_target" {
+  max_capacity       = 2
+  min_capacity       = 1
+  resource_id        = "service/${aws_ecs_cluster.aws-ecs-cluster.name}/${aws_ecs_service.aws-ecs-service.name}"
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace  = "ecs"
+}
+
+resource "aws_appautoscaling_policy" "ecs_policy_memory" {
+  name               = "${var.app_name}-${var.app_environment}-memory-autoscaling"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.ecs_target.resource_id
+  scalable_dimension = aws_appautoscaling_target.ecs_target.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.ecs_target.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageMemoryUtilization"
+    }
+
+    target_value = 80
+  }
+}
+
+resource "aws_appautoscaling_policy" "ecs_policy_cpu" {
+  name               = "${var.app_name}-${var.app_environment}-cpu-autoscaling"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.ecs_target.resource_id
+  scalable_dimension = aws_appautoscaling_target.ecs_target.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.ecs_target.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageCPUUtilization"
+    }
+
+    target_value = 80
+  }
 }
